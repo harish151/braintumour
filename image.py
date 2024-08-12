a6a2392c5af995800213b50879cf73592913c63b
harish151 <125892721+harish151@users.noreply.github.com>
2024-08-12 20:14:29 +0530

    Update image.py

--- image.py
+++ image.py
@@ -3,24 +3,26 @@ from keras._tf_keras.keras.models import load_model
 from keras._tf_keras.keras.preprocessing import image
 import numpy as np
 
-model=load_model("C:/Users/sathish rachuri/OneDrive/Desktop/project2/Model.h5")
-model
+def prediction(path):
+    model=load_model("C:/Users/sathish rachuri/OneDrive/Desktop/project2/Model.h5")
+    #load and preprocess the test image
+    #C:\Users\sathish rachuri\OneDrive\Desktop\project2\uploads\y0.jpg
+    modified_path = path.replace('\\', '/')
+    print(modified_path)
+    test_image_path=f'C:/Users/sathish rachuri/OneDrive/Desktop/project2/'+f'{modified_path}'
+    img=image.load_img(test_image_path,target_size=(224,224))
+    img_array=image.img_to_array(img)
+    img_array=np.expand_dims(img_array,axis=0)
 
+    #add batch dimension
+    img_array/=255.#normalize the pixel values
+    #make predictions
+    prediction=model.predict(img_array)
+    #print the prediction
+    print(prediction)
 
-#load and preprocess the test image
-test_image_path='C:/Users/sathish rachuri/OneDrive/Desktop/project2/Brain_Tumor_Detection-20240301T070908Z-001/Brain_Tumor_Detection/Train/yes/y0.jpg'
-img=image.load_img(test_image_path,target_size=(224,224))
-img_array=image.img_to_array(img)
-img_array=np.expand_dims(img_array,axis=0)
-
-#add batch dimension
-img_array/=255.#normalize the pixel values
-#make predictions
-prediction=model.predict(img_array)
-#print the prediction
-print(prediction)
-
-if prediction < 0.5:
-  print("prediction: No tumor(probability:",prediction[0][0])
-else:
-  print("prediction: Tumor present(probability:",prediction[0][0])
+    if prediction < 0.5:
+        a=f'prediction: No tumor(probability:"{prediction[0][0]})'
+    else:
+        a=f'prediction: Tumor present(probability:{prediction[0][0]})'
+    return a
